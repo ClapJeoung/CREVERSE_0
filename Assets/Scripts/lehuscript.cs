944c5dfdcd45609f092b98386b25c10a6205cb4c
ClapJeoung <89167763+ClapJeoung@users.noreply.github.com>
2024-10-18 17:26:22 +0900

    10.18_2
    두번째커밋

---
+++ Assets/Scripts/lehuscript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class lehuscript : MonoBehaviour
+{
+    [SerializeField] private Transform LehuTrans = null;
+    private Vector3 RandomRot = Vector3.one;
+    void Start()
+    {
+        RandomRot = new Vector3(Random.Range(0, 2) == 0 ? -100.0f: 100.0f, Random.Range(0, 2) == 0 ? -100.0f: 100.0f, Random.Range(0, 2) == 0 ? -100.0f:100.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LehuTrans.Rotate(RandomRot * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Escape))Application.Quit();
+    }
+}
